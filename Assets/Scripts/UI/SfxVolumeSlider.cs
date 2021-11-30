5e982de595aac7b34c6e0b6e95851c0a24ea61b1
deezaath <deezaathps4@gmail.com>
2021-11-30 09:30:50 +0100

    Playerprefs Sound saving system

--- Assets/Scripts/UI/SfxVolumeSlider.cs
+++ Assets/Scripts/UI/SfxVolumeSlider.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SfxVolumeSlider : MonoBehaviour
 {
+
+    private Slider slider;
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        SetSavedLevel();
+    }
+    public float GetCurentLevel()
+    {
+       return  PlayerPref.Instance.GetSFXVolume();
+    }
     public void SFXVolume(float volume)
     {
         AkSoundEngine.SetRTPCValue("SFX_Volume", volume);
+        PlayerPref.Instance.SaveSFXVolume(volume);
+    }
+    public void ResetAll()
+    {
+        PlayerPref.Instance.ResetPlayerSettings();
+    }
+
+
+
+    public void SetSavedLevel()
+    {
+        if (slider)
+        {
+            slider.value = GetCurentLevel();
+        }
+        SFXVolume(GetCurentLevel());
+    }
+    private void OnEnable()
+    {
+        SetSavedLevel();
     }
 }
